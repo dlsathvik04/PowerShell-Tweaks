e12f37513071d1c7a7e7c7c4f00f033428892e09
Lekha Sathvik Devabathini <dlsathvik04@gmail.com>
2023-12-28 21:38:22 +0530

    edited installation script

--- install.ps1
+++ install.ps1
@@ -1,2 +1,3 @@
+New-Item -Path $profile -ItemType "file" -Force
 Invoke-RestMethod https://github.com/dlsathvik04/PowerShell-Tweaks/raw/main/default.ps1 -OutFile $PROFILE
 Invoke-Command { & "pwsh.exe"} -NoNewScope
