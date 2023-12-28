2df9ba75c6657f79603134e23eed878c4c4c56a8
SatXCho <92297743+SatXCho@users.noreply.github.com>
2023-12-28 16:18:11 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,2 +1,2 @@
 Invoke-RestMethod https://github.com/dlsathvik04/PowerShell-Tweaks/raw/main/default.ps1 -OutFile $PROFILE
-Start-Process "$psHome\pwsh.exe"
+Invoke-Command { & "pwsh.exe"} -NoNewScope
