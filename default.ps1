3242c78f082e5ec8460e8b1adf7e6f58fa0a4816
satxcho <satvikcvsss@gmail.com>
2023-12-29 10:08:39 +0530

    added c utils

--- default.ps1
+++ default.ps1
@@ -2,6 +2,10 @@ $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
 $principal = New-Object Security.Principal.WindowsPrincipal $identity
 $isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
+If (Test-Path "C:\ProgramData\miniconda3\Scripts\conda.exe") {
+    (& "C:\ProgramData\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+}
+
 # If so and the current host is a command line, then change to red color 
 # as warning to user that they are operating in an elevated context
 # Useful shortcuts for traversing directories
@@ -51,8 +55,8 @@ function prompt {
         }
     }
     
-    if (Test-CommandExists conda){
-	    Write-Host ($env:CONDA_PROMPT_MODIFIER.Trim()) -nonewline -foregroundcolor darkblue -BackgroundColor DarkYellow
+    if (Test-Path "C:\ProgramData\miniconda3\Scripts\conda.exe"){
+	    Write-Host ($env:CONDA_PROMPT_MODIFIER.Trim()) -nonewline -foregroundcolor darkblue -BackgroundColor DarkYellow -ErrorAction SilentlyContinue
 	}    
 
     if ($isAdmin) {
@@ -218,6 +222,42 @@ function pgrep($name, $id, $search, $port) {
 
 function source($dir) {
     $Env:Path += ";$dir"
+}
+
+function run{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string] $path,
+
+        [Parameter()]
+        [switch] $c,
+
+        [Parameter()]
+        [switch] $cpp,
+
+        [Parameter()]
+        [switch] $t
+    )
+
+    if ($c) {
+        gcc $path -o runc.exe
+        .\runc.exe
+
+    } elseif($cpp){
+        g++ $path -o runc.exe
+        .\runc.exe
+    }
+    else {
+        Write-Host "No Compiler specified"
+    }
+    if ($t){
+        rm .\runc.exe
+    }
+}
+
+function make{
+    mingw32-make
 }
 
 # Manage path here
